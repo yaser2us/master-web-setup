4e95f797acd20d6cc5258d9daae3fc7009298895
yaser2us <yaser2us@gmail.com>
2021-09-15 16:19:27 +0800

    testing api call done ;)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import React, {
+  useCallback, useEffect, useState,
+  useRef
+} from 'react'
+import axios from "axios"
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [messages, setMessages] = useState({});
+
+  useEffect(() => {
+    const fetchMessages = () => {
+        axios.get('https://sit-maya.maybank.com.my/mayasme/mayadb/mayadb/getAllMessages')
+            .then(res => {
+                console.log(res, "formioioioioio, locallll");
+                setMessages(res.data);
+            }).catch(err => console.log(err));
+    }
+    fetchMessages();
+}, [])
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Good JOB ;)</p>       
+        <p>Good JOB ;)</p>
+        <code style={{fontSize:9, color: '#777777', margin: 31}}>
+          {JSON.stringify(messages)}
+        </code>       
       </header>
     </div>
   );
